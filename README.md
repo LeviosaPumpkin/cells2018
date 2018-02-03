b290a988e12c003bc5bf5bf4570ef1e4456bc4a7
LeviosaPumpkin <nastaciia@gmail.com>
2018-02-03 21:58:12 +0300

    started segmentation

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 #to run the program from command promt use 2 arguments: path to a pictyre and path to csv file
+output file_with_colours.csv contains a table with number of kernels of different colour
--- cells/Source.cpp
+++ cells/Source.cpp
@@ -10,124 +10,6 @@
 //2 списка контуров
 //наложить контуры друг на друга
 //иерархия контуров - еще 1 вариант
-using namespace cv;
-using namespace std;
-
-std::fstream file;
-std::vector<std::string> photos_of_cells = { "D://studies//cells//1part//1.jpg",
-	"D://studies//cells//1part//2.jpg",
-	"D://studies//cells//1part//3.jpg", 
-	"D://studies//cells//1part//4.jpg", 
-	"D://studies//cells//1part//8.jpg", 
-	"D://studies//cells//1part//9.jpg", 
-	"D://studies//cells//1part//10.jpg", 
-	"D://studies//cells//1part//11.jpg", 
-	"D://studies//cells//1part//12.jpg", 
-	"D://studies//cells//1part//13.jpg", 
-	"D://studies//cells//1part//14.jpg", 
-	"D://studies//cells//1part//15.jpg", 
-	"D://studies//cells//1part//16.jpg", 
-	"D://studies//cells//1part//17.jpg", 
-	"D://studies//cells//1part//18.jpg", 
-	"D://studies//cells//1part//19.jpg", 
-	"D://studies//cells//1part//20.jpg", 
-	"D://studies//cells//1part//21.jpg", 
-	"D://studies//cells//1part//22.jpg", 
-	"D://studies//cells//1part//23.jpg", 
-	"D://studies//cells//1part//24.jpg", 
-	"D://studies//cells//1part//28.jpg", 
-	"D://studies//cells//1part//29.jpg", 
-	"D://studies//cells//1part//30.jpg", 
-	"D://studies//cells//1part//31.jpg", 
-	"D://studies//cells//1part//33.jpg", 
-	"D://studies//cells//1part//34.jpg", 
-	"D://studies//cells//1part//37.jpg",
-	"D://studies//cells//1part//38.jpg", 
-	"D://studies//cells//1part//39.jpg", 
-	"D://studies//cells//1part//40.jpg", 
-	"D://studies//cells//1part//41.jpg", 
-	"D://studies//cells//1part//43.jpg", 
-	"D://studies//cells//1part//44.jpg", 
-	"D://studies//cells//1part//45.jpg", 
-	"D://studies//cells//1part//46.jpg", 
-	"D://studies//cells//1part//47.jpg", 
-	"D://studies//cells//1part//48.jpg", 
-	"D://studies//cells//1part//52.jpg", 
-	"D://studies//cells//1part//53.jpg", 
-	"D://studies//cells//1part//54.jpg", 
-	"D://studies//cells//1part//55.jpg", 
-	"D://studies//cells//1part//56.jpg", 
-	"D://studies//cells//1part//57.jpg", 
-	"D://studies//cells//1part//58.jpg", 
-	"D://studies//cells//1part//59.jpg", 
-	"D://studies//cells//1part//60.jpg", 
-	"D://studies//cells//1part//61.jpg", 
-	"D://studies//cells//1part//62.jpg", 
-	"D://studies//cells//1part//63.jpg", 
-	"D://studies//cells//1part//65.jpg", 
-	"D://studies//cells//1part//68.jpg", 
-	"D://studies//cells//1part//69.jpg", 
-	"D://studies//cells//1part//71.jpg",
-	"D://studies//cells//1part//72.jpg",
-	"D://studies//cells//1part//73.jpg",
-	"D://studies//cells//1part//75.jpg",
-	"D://studies//cells//1part//76.jpg",
-	"D://studies//cells//1part//77.jpg",
-	"D://studies//cells//1part//79.jpg",
-	"D://studies//cells//1part//85.jpg",
-	"D://studies//cells//1part//86.jpg",
-	"D://studies//cells//1part//87.jpg",
-	"D://studies//cells//1part//88.jpg",
-	"D://studies//cells//1part//89.jpg",
-	"D://studies//cells//1part//90.jpg",
-	"D://studies//cells//1part//91.jpg",
-	"D://studies//cells//1part//92.jpg"
-};
-
-int findCells(const char * filename, cv::Scalar low_color, cv::Scalar high_color)
-{
-	cv::Mat im = imread(filename);
-	imshow("original", im);
-	/*binarisation*/
-	//1
-	cv::Mat hsv_im;
-	cvtColor(im, hsv_im, CV_BGR2GRAY);
-	cv::Mat binary;
-	inRange(hsv_im, low_color, high_color, binary);
-	cv::imshow("bin",binary);
-
-
-	/*borders/*/
-	cv::Mat mStructuringElement = cv::getStructuringElement(CV_SHAPE_ELLIPSE, cv::Size(10, 10));
-	
-	//cv::dilate(binary, binary, mStructuringElement, Point(0, 0), 1, 1, 1);//more white
-	cv::erode(binary, binary, mStructuringElement, cv::Point(-1, -1), 2);//more black
-	cv::imshow("erode", binary);
-	cv::Canny(binary, binary, 50, 200);
-	
-
-	/*count cells*/
-	std::vector<std::vector<cv::Point> > contours;
-	vector<Vec4i> hierarchy;
-	//if (!file) return -1;
-	//file << "imagePath;sample №;classID;centerX;centerY;leftX;topY;rightX;bottomY\n";
-	cv::findContours(binary, contours, hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-	for (int i = 0; i < contours.size(); ++i)
-	{
-		cv::drawContours(im, contours, i, cv::Scalar(0, 200, 0), 1, 8, hierarchy, 0, Point());
-		cv::Rect line_rect = cv::boundingRect(contours[i]);
-		cv::RotatedRect rot_rect = cv::minAreaRect(contours[i]);
-		// Центр фигуры.
-		cv::Point2f center = rot_rect.center;
-		/*file << filename << ";" << filename << ";" << "0;" << rot_rect.center.x << ";" << rot_rect.center.y << ";"
-			<< rot_rect.center.x - rot_rect.size.width / 2 << ";" << rot_rect.center.y - rot_rect.size.height / 2 << ";" 
-			<< rot_rect.center.x + rot_rect.size.width / 2 << ";" << rot_rect.center.y + rot_rect.size.height / 2 << ";" << "\n";*/
-	}
-	imshow("contours", im);
-	//file.close();
-	return contours.size();
-}
-
 
 int main(int argc, const char** argv)
 {
@@ -138,39 +20,49 @@ int main(int argc, const char** argv)
 		filename = (char*)argv[1];
 		outPath = (char*)argv[2];
 	} else{
-		filename = "10.jpg";
+		filename = "D://studies//cells//segmentation//17_R_Prol_40x_dapi_2.jpg";
 		outPath = "result.csv";
 	}
-		
-	//file.open(outPath);
-	std::fstream file_with_colours;
-	file_with_colours.open("file_with_colours.csv");
-	file_with_colours << "imagePath;blue;red;green\n";
-	for (int i = 0; i < photos_of_cells.size(); ++i)
-	{
-		int num = 0;
-		std::cout << "File: " << photos_of_cells[i].c_str() << "\n";
-		file_with_colours << photos_of_cells[i].c_str() << ";";
+	cv::Mat im = cv::imread(filename);
+	imshow("original", im);
 
-		cv::Scalar low_blue = ::Scalar(20, 20, 0); cv::Scalar high_blue = ::Scalar(255, 255, 0);
-		num = findCells(photos_of_cells[i].c_str(), low_blue, high_blue);
-		std::cout << "Num of blue cells: " << num << "\n";
-		file_with_colours << num << ";";
+	//cv::blur(im, im, cv::Size(50, 50));
+	//imshow("blurred", im);
 
-		cv::Scalar low_red = ::Scalar(0, 20, 20); cv::Scalar high_red = cv::Scalar(0, 255, 255);
-		num = findCells(photos_of_cells[i].c_str(), low_red, high_red);
-		std::cout << "Num of red cells: " << num << "\n";
-		file_with_colours << num << ";";
+	cv::Mat p = cv::Mat::zeros(im.cols*im.rows, 5, CV_32F);
+	cv::Mat bestLabels, centers, clustered;
 
-		cv::Scalar low_green = ::Scalar(50, 0, 100); cv::Scalar high_green = cv::Scalar(255, 0, 255);
-		num = findCells(photos_of_cells[i].c_str(), low_green, high_green);
-		std::cout << "Num of green cells: " << num << "\n";
-		file_with_colours << num << ";\n";
-
+	std::vector<cv::Mat> bgr(3);
+	cv::split(im, bgr);
+	for (int i = 0; i<im.cols*im.rows; i++) {
+		p.at<float>(i, 0) = (i / im.cols) / im.rows;
+		p.at<float>(i, 1) = (i%im.cols) / im.cols;
+		p.at<float>(i, 2) = bgr[0].data[i] / 255.0;
+		p.at<float>(i, 3) = bgr[1].data[i] / 255.0;
+		p.at<float>(i, 4) = bgr[2].data[i] / 255.0;
 	}
 
-	file_with_colours.close();
+	int K = 2;
+	cv::kmeans(p, K, bestLabels,
+		cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 10, 1.0),
+		3, cv::KMEANS_PP_CENTERS);
+
+	//int* colors = new int[K];
+	std::vector<int> colors(K);
+	for (int i = 0; i<K; i++) 
+	{
+		colors[i] = 255 / (i + 1);
+	}
 	
+	clustered = cv::Mat(im.rows, im.cols, CV_32F);
+	for (int i = 0; i<im.cols*im.rows; i++) 
+	{
+		clustered.at<float>(i / im.cols, i%im.cols) = (float)(colors[bestLabels.at<int>(i, 0)]);
+	}
+
+	clustered.convertTo(clustered, CV_8U);
+	imshow("clustered", clustered);
+
 	cvWaitKey(0);
 
 	return 0;
